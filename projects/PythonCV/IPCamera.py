b4e70b1d43d4bdc1dd3573c5a184c5704cbae4d6
LeonAgmonNacht <ktuitb@gmail.com>
2016-01-07 20:32:47 +0200

    Update IPCamera.py

--- projects/PythonCV/IPCamera.py
+++ projects/PythonCV/IPCamera.py
@@ -10,13 +10,17 @@ class IPCamera():
     IPCamera- This class handles all the needed variables and methods for the camera (python side) of the project.
     '''
 
-    def __init__(self, path, minimum_bounding_size, min_color, max_color):
+    def __init__(self, path, flag_file_path, results_file_path, minimum_bounding_size, min_color, max_color):
         '''
         This method initialising the variables for the IPCamera instance.
         :param path: The FTP path for the current jpeg image (the jpeg image should be updated using the roborio code).
         :param minimum_bounding_size: The minimum contour area to be considered as an object.
         :param max_color: The max color to be passed in the mask. (in the format: np.array([R, G, B])
         :param min_color: The min color to be passed in the mask. (in the format: np.array([R, G, B])
+        :param results_file_path: The path for the results to be used by the java process.
+        :param flag_file_path: The path for the flags:
+            - First line: Should read next image(1), if first line is 0 the java process should write image.
+                          After finish reading image should change flag to 0.
         :return: None
         '''
         self.path = path
@@ -31,6 +35,9 @@ class IPCamera():
         self.min_color = min_color
         self.image_width = None
         self.image_height = None
+        self.flag_file_path = flag_file_path
+        self.results_file_path = results_file_path
+        self.masked_image = None
         # The scales of the object in self.current_image, should be updated using self.update_object_scales.
         self.object_width = None
         self.object_height = None
@@ -61,7 +68,7 @@ class IPCamera():
         self.known_distance = distance
         self.known_width = width
 
-        self.focalLength = (marker[1][0] * distance) / width
+        self.focal_length = (marker[1][0] * distance) / width
 
 
     def set_image_scales(self):
@@ -86,10 +93,15 @@ class IPCamera():
         if self.current_image is not None and self.image_width is None and self.image_height is None:
             self.set_image_scales()
 
-        req = urllib.urlopen(self.path)
-        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        img = cv2.imdecode(arr, -1)
-        self.current_image = img  # The current image in the instance path. (already in cv2 format).
+        with open(self.flag_file_path, "r") as f:
+            flag = f.readline().strip()
+        #if flag == "1": # Should read image.
+        if True:
+            req = urllib.urlopen(self.path)
+            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+            img = cv2.imdecode(arr, -1)
+            self.current_image = img  # The current image in the instance path. (already in cv2 format).
+            self.update_flag_file()
 
     def update_object_scales(self):
         '''
@@ -101,6 +113,24 @@ class IPCamera():
         self.object_width = w
         self.object__x = x
         self.object_y = y
+
+    def update_results_file(self, results):
+        '''
+        This method updates the result in self.results_file.
+        :param results: The result to be written.
+        :return: None.
+        '''
+        result_file = open(self.results_file_path, "w")
+        result_file.write(str(results))
+        result_file.close()
+
+    def update_flag_file(self):
+        '''
+        This method sets the flag file to 0.
+        :return: None.
+        '''
+        with open(self.flag_file_path, "w") as f:
+            f.write("1")
 
     # MARK: Calculating Methods.
 
@@ -116,7 +146,7 @@ class IPCamera():
 
         # Bitwise-AND mask and original image.
         res = cv2.bitwise_and(self.current_image, self.current_image, mask= mask)
-
+        self.masked_image = res
         thresh = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)[1]
 
         # dilate the thresholded image to fill in holes, then find contours on thresholded image.
@@ -135,5 +165,25 @@ class IPCamera():
         Should call self.update_object_scales before.
         :return: The distance of the object from the camera.
         '''
-        return (self.known_width * self.focal_ength) / self.object_width
+        return float(self.known_width * self.focal_length) / float(self.object_width)
 
+    # MARK: Getters.
+
+    def get_object_center(self):
+        '''
+        This method returns the center of the object on the screen (x,y).
+        :return: The center of the object, in the format of (x,y)
+        '''
+        x = self.object_x + self.object_width/2
+        y = self.object_y + self.object_height/2
+        return (x,y)
+
+    def get_distance_from_frame_center(self):
+        '''
+        This method calculates the distance (from center) of the object from the center of the frame.
+        :return: The distance of the object from the center of the frame.
+        '''
+        frame_center = self.image_width/2
+        return frame_center - (self.object_x + self.object_width/2)
+        # If the object is left from the center of the frame,
+        # the result will be positive.
